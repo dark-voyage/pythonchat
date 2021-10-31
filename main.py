b915bdd1910513f080bd4683615cefdf8fee6da3
Geno Ferollo <sakhib.orzklv@icloud.com>
2021-10-31 21:31:58 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pywebio.input import *
 from pywebio.output import *
 from pywebio.platform import tornado
 from pywebio.session import base, defer_call, info as session_info, run_async, run_js
-import os.path
+import os
 
 chat_msgs = []
 online_users = set()
@@ -66,6 +66,5 @@ async def refresh_msg(nickname, msg_box):
             chat_msgs = chat_msgs[len(chat_msgs) // 2:]
         
         last_idx = len(chat_msgs)
-port1 = int(os.environ.get('PORT', 17995))
 if __name__ == "__main__":
-    pywebio.platform.tornado_http.start_server(main, port=port1, host='chatforus.herokuapp.com', debug=False, cdn=True, static_dir=None, allowed_origins=None, check_origin=None, auto_open_webbrowser=False, session_expire_seconds=None, session_cleanup_interval=None, max_payload_size='200M')
+    pywebio.platform.tornado_http.start_server(main, port=17995, host='chatforus.herokuapp.com', debug=False, cdn=True, static_dir=None, allowed_origins=None, check_origin=None, auto_open_webbrowser=False, session_expire_seconds=None, session_cleanup_interval=None, max_payload_size='200M')
